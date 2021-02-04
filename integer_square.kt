e3da09e8c3853bc08009abd42f19556b61792b52
noion <noion0511@gmail.com>
2021-02-04 18:44:25 +0900

    정수제곱근 판별

--- integer_square.kt
+++ integer_square.kt
@@ -3,7 +3,23 @@ class Solution {
         var answer: Long = -1
         var num:Long = 1
         
-        while(n >= num && n > 0) {
+        while(n >= num) {
+            if(n == num*num) {
+                answer = (num+1)*(num+1)
+                return answer
+            }
+            num = num + 1
+        }
+        return answer
+    }
+}
+
+"""class Solution {
+    fun solution(n: Long): Long {
+        var answer: Long = -1
+        var num:Long = 1
+        
+        while(n >= num) {
             if(n/num == num) {
                 answer = (num+1)*(num+1)
                 return answer
@@ -13,8 +29,10 @@ class Solution {
         return answer
     }
 }
-"""
     테스트케이스 13번이 안되는데 이유를 모르겠음,, 
-    그래서 Math.sqrt(),pow()를 활용하기로 함
+    그래서 Math.sqrt(),pow()를 활용하기로 함 
+
+    n/num == num인 조건이 문제 였음,, 5/2==2등이 만족한 듯?
+    n = num*num으로 변경
 """
 
